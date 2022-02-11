71e2d3d2404ffc3b6e2854c0560a910ac6b25aa7
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-11 17:28:40 -0600

    playlist genre search route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get '/playlist/:playlist_id', to: 'playlist#show'
   
   get '/genre', to: 'genre#index'
-  get '/genre/:genre_id', to: 'genre#show'
+  get '/genre/:genre', to: 'genre#show'
 
-  get '/genre/:genre_id/playlists', to: 'playlist#genre_search'
-  get '/playlists/genre/nil', to: 'playlist#nil_genre'
+  get '/genre/:genre/playlists', to: 'playlist#genre_search'
+
+  post '/playlist/new', to: 'playlist#new'
 end
