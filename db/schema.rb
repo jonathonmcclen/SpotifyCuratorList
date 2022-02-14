522d04d2bb9785bf4c51d54202ebe08c65bafd22
--replace-all <jonathonsmcclendon@gmail.com>
2022-02-14 16:41:36 -0600

    switched to postgresql
    removed sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
   end
